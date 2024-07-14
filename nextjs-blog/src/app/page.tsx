db325d7731f6c6299483b7f8e3e55ba5c5d0d1e4
AlanThomasJohn <alan.john@bytstrone.com>
2024-07-14 23:16:15 +0530

    final blog project

---
+++ nextjs-blog/src/app/page.tsx
@@ -0,0 +1,46 @@
+import Blogs from "@/components/Blogs";
+import Categories from "@/components/Categories";
+import Image from "next/image";
+
+async function fetchCategories(){
+  const options = {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
+    }
+  };
+  try{
+    const res  = await fetch("http://127.0.0.1:1337/api/categories");
+    const response = await res.json();
+    return response;
+  } catch(err){
+    console.error(err);
+  }
+}
+
+async function fetchBlogs(){
+  const options = {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`
+    }
+  };
+  try{
+    const res  = await fetch("http://127.0.0.1:1337/api/blogs?populate=*");
+    const response = await res.json();
+    return response;
+  } catch(err){
+    console.error(err);
+  }
+}
+
+
+export default async function Home() {
+  const categories = await fetchCategories();
+  const blogs = await fetchBlogs();
+  return (
+  <div>
+    <Categories  categories={categories}/>
+    <Blogs blogs={blogs}/>
+  </div>
+  );
+
+}
